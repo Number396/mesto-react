613bd04a0a3e4363b7bbc1e9d2dc58b62801d303
Number396 <number396@yandex.ru>
2022-12-28 20:27:22 +0300

    refactor: delete unused constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,36 +1,3 @@
-// export const popupEdit = document.querySelector(".popup_type_edit");
-// export const popupPlace = document.querySelector(".popup_type_add");
-// export const popupUpdateAvatar = document.querySelector(".popup_type_avatar");
-// export const profile = document.querySelector(".profile");
-// export const profileEditBtn = profile.querySelector(".profile__edit-button");
-// export const newcardAddBtn = profile.querySelector(".profile__add-button");
-// export const updateAvatarBtn = profile.querySelector(".profile__avatar-button");
-
-// export const formEditElement = popupEdit.querySelector("form");
-// export const formAddElement = popupPlace.querySelector("form");
-// export const formUpdateAvatar = popupUpdateAvatar.querySelector("form");
-
-// export const settings = {
-//     formSelector: ".popup__form",
-//     fieldsetSelector: ".popup__set",
-//     inputSelector: ".popup__input",
-//     submitButtonSelector: ".popup__submit-button",
-//     inactiveButtonClass: "popup__button_inactive",
-//     inputErrorClass: "popup__input_type_error",
-//     errorClass: "popup__input-error_active",
-// };
-
-// export const profileSelectors = {
-//     title: ".profile__title",
-//     subtitle: ".profile__subtitle",
-//     avatar: ".profile__avatar"
-// };
-// //мапа: (ключ: форма, значение: экземпляр класса валидации для этой формы)
-// export const formsCollection = new Map();
-// export const formList = Array.from(
-//     document.querySelectorAll(settings.formSelector)
-// );
-
 export const apiConfig = {
     url: "https://mesto.nomoreparties.co/v1/cohort-54",
     headers: {
